dad27190cce9278b41ca137fa962447709c79c7e
MansiSNawlani <mansishankarnawlani@gmail.com>
2026-05-07 23:53:05 +0530

    changes to setup onboarding page

---
+++ src/app/onboarding/_client.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+import { Loader2Icon } from "lucide-react"
+
+export function OnboardingClient({userId}){
+    return <Loader2Icon className="animate-spin size-24"/>
+
+}
--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,3 +1,18 @@
+import { getCurrentUser } from "@/services/clerk/lib/getCurrentUser";
+import { redirect } from "next/navigation";
+
 export default async function OnboardingPage() {
  const { userId, user } = await getCurrentUser({allData: true});
+ if(userId == null) {
+    return redirect("/");
+ }
+ if(user != null) {
+    return redirect("/app");
+ }
+
+ return (
+ <div className="container flex flex-col items-center justify-center h-screen gap-4">
+    <h1 className="text-4xl">Creating your account...</h1>
+</div>
+ );
 }
