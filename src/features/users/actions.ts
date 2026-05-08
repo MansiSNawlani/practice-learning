4985b55511d284bda29e601b51ec652af38400fc
MansiSNawlani <mansishankarnawlani@gmail.com>
2026-05-09 00:12:00 +0530

    changes to setup onboarding page

---
+++ src/features/users/actions.ts
@@ -0,0 +1,15 @@
+"use server"
+
+import { cacheTag } from "next/dist/server/use-cache/cache-tag"
+import { getUserIdTag } from "./dbCache"
+import { db } from "@/drizzle/db";
+import { UserTable } from "@/drizzle/schema/user";
+import { eq } from "drizzle-orm";
+
+export async function getUser(id: string) {
+    "use cache"
+    cacheTag(getUserIdTag(id));
+    return db.query.UserTable.findFirst({   
+        where: eq(UserTable.id, id),
+    })
+}
