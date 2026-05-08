20ab528f20d2931c22cc8c50b49a332bf5dcd728
MansiSNawlani <mansishankarnawlani@gmail.com>
2026-05-08 23:59:44 +0530

    changes to setup onboarding page

--- src/app/onboarding/_client.tsx
+++ src/app/onboarding/_client.tsx
@@ -1,8 +1,19 @@
 "use client"
 
 import { Loader2Icon } from "lucide-react"
+import { useRouter } from "next/navigation";
+import { useEffect } from "react"
 
-export function OnboardingClient({userId}){
+export function  OnboardingClient({userId}){
+    const router = useRouter();
+    useEffect(()=> {
+        const intervalId = setInterval(async () => {
+            const user = await getUsers(userId)
+            if (user == null) return
+
+            router.replace("/app")
+        }, 250)
+    }, [userId])
     return <Loader2Icon className="animate-spin size-24"/>
 
 }
